7e1c6b39af2805e55b09e21640bd42eb545e9c42
TuckerO33 <tuckerovermann@gmail.com>
2024-04-03 11:06:33 -0500

    functions exercises

--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -1,5 +1,4 @@
 function reverse(str) {
-   let lettersArray = str.split('');
-   let reversedLettersArray = lettersArray.reverse();
-   return reversedLettersArray.join('');
+   return str.split('').reverse().join('');
 }
+console.log(reverse("Tucker"))
